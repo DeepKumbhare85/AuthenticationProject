87f03b1431069c5ec45bd5c53b578ad4a4a4a18a
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-03-31 19:45:38 +0530

    refresh_token

--- authServer.js
+++ authServer.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 
 const jwt = require('jsonwebtoken');
-
 app.use(express.json());
 
+let refreshTokens = [];
+
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' });
+}
 
 app.post('/login', (req, res) => {
     //authentication of user 
@@ -13,9 +18,24 @@ app.post('/login', (req, res) => {
     const username = req.body.username;
     const user = { name: username };
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-    res.json({ accessToken: accessToken })
+    const accessToken = generateAccessToken(user);
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    refreshTokens.push(refreshToken);
+    res.json({ accessToken: accessToken, refreshToken: refreshToken })
 })
 
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token
+    console.log(refreshTokens, refreshToken)
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        console.log(err)
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({ name: user.name })
+        res.json({ "accessToken": accessToken });
+    })
+
+})
 
 app.listen(4000);
